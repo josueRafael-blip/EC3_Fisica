a3191bc8df5bd7968ed1f3b3334750c8454b351e
josueRafael-blip <josueisraelrafaelrojas9@gmail.com>
2026-07-07 18:58:05 -0500

    Update visual.py

--- module/visual.py
+++ module/visual.py
@@ -1,1 +1,46 @@
+def dibujar_bidon(porcentaje, longitud_chorro):
 
+    agua = f"""
+    <div style="
+        position:absolute;
+        bottom:0;
+        width:100%;
+        height:{porcentaje}%;
+        background:#2196F3;
+        transition:1s;
+    "></div>
+    """
+
+    html = f"""
+
+    <div style="display:flex;align-items:center;">
+
+        <div style="
+            width:220px;
+            height:420px;
+            border:5px solid white;
+            border-radius:20px;
+            position:relative;
+            overflow:hidden;
+            background:#ececec;
+        ">
+
+            {agua}
+
+        </div>
+
+        <div style="
+            width:{longitud_chorro}px;
+            height:8px;
+            background:#4FC3F7;
+            margin-left:5px;
+            border-radius:10px;
+        ">
+
+        </div>
+
+    </div>
+
+    """
+
+    return html
