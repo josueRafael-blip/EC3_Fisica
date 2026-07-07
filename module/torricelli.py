20f8f21386d0af3fe2872a0c64c498ef28543a30
josueRafael-blip <josueisraelrafaelrojas9@gmail.com>
2026-07-07 18:14:04 -0500

    Update torricelli.py

--- module/torricelli.py
+++ module/torricelli.py
@@ -1,1 +1,18 @@
+import math
 
+g = 9.81
+
+rho = 1000
+
+def velocidad(altura):
+    return math.sqrt(2 * g * altura)
+
+def area(diametro):
+    radio = diametro / 2
+    return math.pi * radio**2
+
+def caudal(altura, diametro):
+    return area(diametro) * velocidad(altura)
+
+def presion(altura):
+    return rho * g * altura
