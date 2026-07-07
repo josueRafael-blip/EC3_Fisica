5953cf8d768c22b56e8cba7720a83f846f8a9383
josueRafael-blip <josueisraelrafaelrojas9@gmail.com>
2026-07-07 18:14:24 -0500

    Update 3_Simulador.py

--- pages/3_Simulador.py
+++ pages/3_Simulador.py
@@ -1,1 +1,38 @@
+import streamlit as st
 
+from modules.torricelli import *
+
+st.title("🧪 Simulador de Torricelli")
+
+altura = st.slider(
+    "Altura del agua (cm)",
+    1,
+    100,
+    30
+)
+
+diametro = st.slider(
+    "Diámetro del orificio (mm)",
+    1,
+    20,
+    4
+)
+
+altura = altura / 100
+diametro = diametro / 1000
+
+if st.button("Calcular"):
+
+    v = velocidad(altura)
+
+    q = caudal(altura, diametro)
+
+    p = presion(altura)
+
+    c1,c2,c3 = st.columns(3)
+
+    c1.metric("🚀 Velocidad",f"{v:.3f} m/s")
+
+    c2.metric("🌊 Caudal",f"{q:.8f} m³/s")
+
+    c3.metric("⚙ Presión",f"{p:.2f} Pa")
