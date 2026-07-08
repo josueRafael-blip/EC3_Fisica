0da5a1abc80433ac42752ef31f5a6cdca382ad3f
josueRafael-blip <josueisraelrafaelrojas9@gmail.com>
2026-07-07 22:44:30 -0500

    Update visual.py

--- module/visual.py
+++ module/visual.py
@@ -1,9 +1,9 @@
 def dibujar_bidon(porcentaje, longitud_chorro):
     porcentaje = max(0, min(100, porcentaje))
-    longitud_chorro = max(20, min(220, longitud_chorro))
+    longitud_chorro = max(8, min(220, longitud_chorro))
 
     html = f"""
-    <div style="display:flex; align-items:center; gap:0px; margin-top:20px;">
+    <div style="display:flex; align-items:center; gap:0px; margin-top:10px;">
 
         <div style="
             width:220px;
@@ -14,13 +14,15 @@ def dibujar_bidon(porcentaje, longitud_chorro):
             overflow:hidden;
             background:#E8F4FA;
         ">
+
             <div style="
                 position:absolute;
                 bottom:0;
                 left:0;
                 width:100%;
                 height:{porcentaje}%;
-                background:#2196F3;
+                background:linear-gradient(180deg, #42A5F5, #1E88E5);
+                transition:height 0.4s ease;
             "></div>
 
             <div style="
@@ -34,6 +36,19 @@ def dibujar_bidon(porcentaje, longitud_chorro):
                 border:3px solid white;
                 z-index:5;
             "></div>
+
+            <div style="
+                position:absolute;
+                top:10px;
+                left:15px;
+                color:#0F172A;
+                font-weight:bold;
+                font-size:14px;
+                z-index:6;
+            ">
+                {porcentaje:.1f}%
+            </div>
+
         </div>
 
         <div style="
@@ -42,6 +57,7 @@ def dibujar_bidon(porcentaje, longitud_chorro):
             background:#4FC3F7;
             border-radius:10px;
             margin-top:250px;
+            transition:width 0.4s ease;
         "></div>
 
     </div>
